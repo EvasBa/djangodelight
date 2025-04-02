f4f00c8ea700244ed6b8417036849175c992c12e
EvasBa <evaldas.bartauskis@gmail.com>
2025-04-02 08:05:43 +0300

    Update .gitignore

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files configuration (Django 5.1+)
+STATIC_URL = 'static/'  # URL to access static files
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Additional directories for static files
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Directory for collected static files in production
+
+# Media files configuration (if needed)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
